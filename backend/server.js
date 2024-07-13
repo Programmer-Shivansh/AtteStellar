a92bd223ed81c21d039df333f2c8cc17623834d9
Programmer-Shivansh <shivanshchauhan2005@gmail.com>
2024-07-13 18:45:50 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://atte-stellar-6f5xdjfmd-programmer-shivanshs-projects.vercel.app/", // Allow only this origin
+    origin: "https://atte-stellar-hv1fgowyh-programmer-shivanshs-projects.vercel.app/", // Allow only this origin
   })
 );
+
 
 mongoose
   .connect(process.env.MONGO_URI)
